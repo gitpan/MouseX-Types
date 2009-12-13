d4523ae0292e94694dd05a8e55cd835172735788
藤 吾郎 <gfuji@cpan.org>
2009-12-13 07:41:58 +0000

    Import of GFUJI/MouseX-Types-0.02_01 from CPAN.
    gitpan-cpan-distribution: MouseX-Types
    gitpan-cpan-version:      0.02_01
    gitpan-cpan-path:         GFUJI/MouseX-Types-0.02_01.tar.gz
    gitpan-cpan-author:       GFUJI
    gitpan-cpan-maturity:     developer

--- inc/Module/Install/Repository.pm
+++ inc/Module/Install/Repository.pm
@@ -2,17 +2,23 @@
 package Module::Install::Repository;
 
 use strict;
-use 5.008_001;
-our $VERSION = '0.02';
+use 5.005;
+use vars qw($VERSION);
+$VERSION = '0.05';
 
 use base qw(Module::Install::Base);
+
+sub _execute {
+    my ($command) = @_;
+    `$command`;
+}
 
 sub auto_set_repository {
     my $self = shift;
 
     return unless $Module::Install::AUTHOR;
 
-    my $repo = _find_repo();
+    my $repo = _find_repo(\&_execute);
     if ($repo) {
         $self->repository($repo);
     } else {
@@ -21,13 +27,17 @@ sub auto_set_repository {
 }
 
 sub _find_repo {
+    my ($execute) = @_;
+
     if (-e ".git") {
         # TODO support remote besides 'origin'?
-        if (`git remote show origin` =~ /URL: (.*)$/m) {
+        if ($execute->('git remote show -n origin') =~ /URL: (.*)$/m) {
             # XXX Make it public clone URL, but this only works with github
             my $git_url = $1;
             $git_url =~ s![\w\-]+\@([^:]+):!git://$1/!;
             return $git_url;
+        } elsif ($execute->('git svn info') =~ /URL: (.*)$/m) {
+            return $1;
         }
     } elsif (-e ".svn") {
         if (`svn info` =~ /URL: (.*)$/m) {
@@ -45,6 +55,12 @@ sub _find_repo {
         while (<$handle>) {
             chomp;
             return $_ if m!^http://!;
+        }
+    } elsif (-e ".hg") {
+        if ($execute->('hg paths') =~ /default = (.*)$/m) {
+            my $mercurial_url = $1;
+            $mercurial_url =~ s!^ssh://hg\@(bitbucket\.org/)!https://$1!;
+            return $mercurial_url;
         }
     } elsif (-e "$ENV{HOME}/.svk") {
         # Is there an explicit way to check if it's an svk checkout?
@@ -69,4 +85,4 @@ __END__
 
 =encoding utf-8
 
-#line 110
+#line 128
